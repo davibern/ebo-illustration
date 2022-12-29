4cc4e3e890ba2cd372a75f60e0d209a7961a9c71
David Bernabé <david.bern.pal@gmail.com>
2022-12-29 20:57:15 +0100

    Añadir función para enviar correo

--- email.php
+++ email.php
@@ -1,9 +1,13 @@
 <?php
 
-    if(isset($_POST['send'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email']
-        $message = $_POST['message']
+    $to = 'david.bern.pal@gmail.com';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-        mail($email, $name, $message)
-    }
+    $header = "Mensaje enviado desde la web";
+    //$completeMessage = "De: ".$name."\nEmail: ".$email."\n\nMensaje: ".$message;
+
+    mail($to, $header, $message);
+
+   echo "<script>console.log('{$message}');</script>";
